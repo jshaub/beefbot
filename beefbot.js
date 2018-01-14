00fab96329b529636b49c7a39e9f844aa7d15a97
Jonathan Shaub <shauby19@gmail.com>
2018-01-13 21:04:25 -0500

    Added getBase (working) and setBase (not working) functions

--- beefbot.js
+++ beefbot.js
@@ -1,15 +1,63 @@
+/* --------------------- *
+ * --------------------- *
+ * VARIABLE DECLARATIONS *
+ * --------------------- *
+ * --------------------- */
+
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-// Log bot into server
-bot.login("NDAxNjE3MTA3MDcyMDU3MzQ1.DTsytw.oAixuGMjdArrLgoIBO-fqiEzoqk");
+baseCoordX = null;
+baseCoordY = null;
 
+/* ------------ *
+ * ------------ *
+ * MAIN PROGRAM *
+ * ------------ *
+ * ------------ */
 
-bot.on("ready", () => { console.log("BeefBot is running..."); } );
+startBot("NDAxNjE3MTA3MDcyMDU3MzQ1.DTsytw.oAixuGMjdArrLgoIBO-fqiEzoqk");
 
-bot.on("message", (message) => {
-		if(message.content.startsWith("!ping")) {
-			console.log("Received !ping");
-			message.channel.send("pong!");
+bot.on("ready", () => 
+	{
+		console.log("BeefBot is running...");
+	}
+);
+
+bot.on("message", (message) => 
+	{
+		if(message.content === ("!base")) {
+			getBase(message);
+		} else if(message.content === ("!setBase")) {
+			setBase(message);
 		}
-		} );
+	}	
+);
+
+/* --------------------- *
+ * --------------------- *
+ * FUNCTION DECLARATIONS *
+ * --------------------- *
+ * --------------------- */
+
+// Starts the bot
+function startBot(token) {
+	bot.login(token);
+}
+
+// Set base coordinates to given x and y
+function setBase(x, y) {
+	console.log("Setting new base coords");
+	baseCoordX = x;
+	baseCoordY = y;
+	message.channel.send("Base coordinates updated");
+}
+
+// Prints out base coordinates, or an error if they've not yet been set
+function getBase(message) {
+	if(baseCoordX == null) {
+		message.reply(" base coordinates are not set. Please contact an admin to set them");
+	} else {
+		message.reply(", 7D2D base is at X: " + baseCoordX + ", Y: " + baseCoordY);
+	}
+}
